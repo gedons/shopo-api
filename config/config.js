6efa77f0ec0486aa04043bd5d870b2247d839272
Nerdy Gedoni <gedoniani@gmail.com>
2023-11-13 15:14:40 +0100

    config file protected

--- config/config.js
+++ config/config.js
@@ -1,4 +1,6 @@
+const env = require('dotenv').config();
+
 module.exports = {
-    mongoURI: "mongodb+srv://gedoniani:blues0001153@cluster0.kr2axsg.mongodb.net/shop-api?retryWrites=true&w=majority",
-    SESSION_SECRET: "ergehrtrt444rj76h54576rssqaj658"
+    mongoURI: process.env.mongoURI, 
+    SESSION_SECRET: process.env.SESSION_SECRET
 };
