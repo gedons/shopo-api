4103386ecf1385c3307d99c20eec74ecab4383cc
Nerdy Gedoni <gedoniani@gmail.com>
2023-12-05 14:42:11 +0100

    email config

--- config/EmailConfig.js
+++ config/EmailConfig.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     host: "smtp.titan.email",
-    port: 587,
+    port: 465,
     auth: {
         user:'admin@daeds.uk',
         pass: 'P@xwd2023'
