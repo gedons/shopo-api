adffd37ec2c5b7f8f733363c8014bd7efbabc849
Nerdy Gedoni <gedoniani@gmail.com>
2023-12-11 10:55:50 +0100

    update

--- controllers/productController.js
+++ controllers/productController.js
@@ -33,7 +33,7 @@ exports.createProduct = async (req, res) => {
 
       // Function to upload images to Google Cloud Storage
       const uploadPromises = images.map((image) => {
-      const imageFileName = `${Date.now()}-${image.originalname}`;
+      const imageFileName = image.originalname;
       const file = bucket.file(`${imageFileName}`);
       return file.save(image.buffer, { contentType: image.mimetype });
     });
@@ -174,52 +174,52 @@ exports.updateProductById = async (req, res) => {
   }
 };
 
-// Handle image upload for a specific product by ID
-exports.uploadProductImage = async (req, res) => {
-  try {
-    const productId = req.params.productId;
-    const product = await Product.findById(productId);
+  // Handle image upload for a specific product by ID
+  exports.uploadProductImage = async (req, res) => {
+    try {
+      const productId = req.params.productId;
+      const product = await Product.findById(productId);
 
-    if (!product) {
-      return res.status(404).json({ message: 'Product not found' });
+      if (!product) {
+        return res.status(404).json({ message: 'Product not found' });
+      }
+
+      if (!req.file) {
+        return res.status(400).json({ message: 'No image file uploaded' });
+      }
+
+      const fileName = req.file.originalname;
+      const file = bucket.file(fileName);
+
+      // Create a write stream to upload the file
+      const stream = file.createWriteStream({
+        metadata: {
+          contentType: req.file.mimetype,
+        },
+        resumable: false,
+      });
+
+      stream.on('error', (err) => {
+        console.error('Error uploading to GCS:', err);
+        res.status(500).json({ message: 'Failed to upload image', error: err.message });
+      });
+
+      stream.on('finish', async () => {
+        // Once the file is successfully uploaded, add its GCS path to the product's images array
+        const gcsImagePath = `https://storage.googleapis.com/project-molding_bucket/${fileName}`;
+        product.images.push(gcsImagePath);
+
+        const updatedProduct = await product.save();
+
+        res.status(200).json({ message: 'Image added successfully', image: updatedProduct });
+      });
+
+      stream.end(req.file.buffer);  
+    } catch (error) {
+      console.error('Error uploading product image:', error);
+      res.status(500).json({ message: 'Failed to upload image', error: error.message });
     }
-
-    if (!req.file) {
-      return res.status(400).json({ message: 'No image file uploaded' });
-    }
-
-    const fileName = `${path.basename(req.file.originalname)}`;
-    const file = bucket.file(fileName);
-
-    // Create a write stream to upload the file
-    const stream = file.createWriteStream({
-      metadata: {
-        contentType: req.file.mimetype,
-      },
-      resumable: false,
-    });
-
-    stream.on('error', (err) => {
-      console.error('Error uploading to GCS:', err);
-      res.status(500).json({ message: 'Failed to upload image', error: err.message });
-    });
-
-    stream.on('finish', async () => {
-      // Once the file is successfully uploaded, add its GCS path to the product's images array
-      const gcsImagePath = `https://storage.googleapis.com/project-molding_bucket/${fileName}`;
-      product.images.push(gcsImagePath);
-
-      const updatedProduct = await product.save();
-
-      res.status(200).json({ message: 'Image added successfully', image: updatedProduct });
-    });
-
-    stream.end(req.file.buffer);  
-  } catch (error) {
-    console.error('Error uploading product image:', error);
-    res.status(500).json({ message: 'Failed to upload image', error: error.message });
-  }
-};
+  };
 
 
 // Delete a product by ID
